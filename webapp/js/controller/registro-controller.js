4935b45eabef41e3c3df8b9b4141a3b4c49fa348
benji Castillo <castilloeguezbenji@gmail.com>
2017-06-04 10:39:18 -0400

    Registro user, terminado

--- webapp/js/controller/registro-controller.js
+++ webapp/js/controller/registro-controller.js
@@ -1,17 +1,12 @@
 var app = angular.module('jscrumApp.registroCtrl',[]);
 
-app.controller('registroCtrl', ['$scope','userServices' ,function($scope,userServices){
+app.controller('registroCtrl', ['$scope','userServices','$window' ,function($scope,userServices,$window){
 	
-    // $scope.registro = "Este es el registro";
-    $scope.user = {};
-    // $scope.user.name = "";
-    // $scope.user.lasname = "";
-    // $scope.user.email = "";
-    // $scope.user.nick = "";
-    // $scope.user.password = "";
-    // $scope.user.profession = "";
-    $scope.user.visible = false;
 
+    $scope.user = {};
+
+    $scope.user.visible = false;
+    $scope.user.respuesta = "";
     $scope.registrar = function(user){
         
 			// console.log(user);
@@ -20,10 +15,31 @@ app.controller('registroCtrl', ['$scope','userServices' ,function($scope,userSer
             $scope.user.visible = true;
 			userServices.insertar(user).then(function(){
 				$scope.response = userServices.response;
-				console.log($scope.response)
-                if($scope.response.response){
+				//console.log($scope.response.message)
+                if($scope.response.message == "0"){
                     $scope.user.visible = false;
                     console.log($scope.user.visible);
+                    // console.log("correo invalido");
+                    // console.log("El correo ya fue registrado");
+                    $scope.user.respuesta = "El correo ya fue registrado, intente otro";
+                    
+
+                }
+                if($scope.response.message == "1"){
+                    $scope.user.visible = false;
+                    console.log($scope.user.visible);
+                    console.log("INsertado correctament");
+                    $scope.user.respuesta = "Registro Relizado Correctamente";
+
+                    setTimeout(function() {
+                        $window.location.href = '#/form1.html';
+                    }, 2000);
+                }
+                if($scope.response.message == "2"){
+                    $scope.user.visible = false;
+                    $scope.user.respuesta = "El usuario ya fue registrado, intente otro";
+                    console.log($scope.user.visible);
+                    console.log("Usuario incorrecto");
                 }
 			});
     }
