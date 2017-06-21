3bec350087f7b637b3c5571b965ab77fd8cddf77
benji Castillo <castilloeguezbenji@gmail.com>
2017-06-21 19:55:26 -0400

    vista sprints

--- webapp/js/controller/manager-proyecto-controller.js
+++ webapp/js/controller/manager-proyecto-controller.js
@@ -9,7 +9,7 @@ app.controller('managerProyectoCtrl', ['$scope','$filter','$window','$sessionSto
     $scope.verpila2 =true;
     $scope.verSprint = false;
     $scope.verSprint2 = true;
-    console.log($scope.idProyecto);  
+        // console.log($scope.idProyecto);  
 
     //fecha
     	
@@ -73,7 +73,7 @@ app.controller('managerProyectoCtrl', ['$scope','$filter','$window','$sessionSto
 
     $scope.insertarPila = function(pila){
         
-			console.log(pila);
+			// console.log(pila);
 
 			equipoServices.insertarPila(pila).then(function(){
 				$scope.pilaInsertada = equipoServices.response;
@@ -88,20 +88,21 @@ app.controller('managerProyectoCtrl', ['$scope','$filter','$window','$sessionSto
 
 			//console.log(sprint);
            sprint.fechaEntrega = $scope.convertDate(sprint.fechaEntrega)
-           console.log(sprint)
-			// equipoServices.insertarPila(pila).then(function(){
-			// 	$scope.pilaInsertada = equipoServices.response;
-            //     // console.log($scope.pilaInsertada);
-            //      $("#modal-insertar-pila").modal("hide");
-            //     //  console.log($scope.id_equipo);
-            //      $scope.listarPila($scope.idProyecto);
-			// });
+           sprint.idProyecto = $scope.idProyecto;
+           console.log(sprint) 
+			equipoServices.insertarSprint(sprint).then(function(){
+				$scope.sprintInsertado = equipoServices.response;
+                 console.log($scope.sprintInsertado);
+                 $("#modal-insertar-sprint").modal("hide");
+                
+                 $scope.listarSprint($scope.idProyecto);
+			});
     }  
 
     $scope.listarSprint = function(id){
         equipoServices.listarSprint(id).then(function(){
 				$scope.sprints = equipoServices.response.message;
-                console.log($scope.sprints)
+                // console.log($scope.sprints)
                 if($scope.sprints[0].respuesta){
                     console.log("No existen Sprints")
                 }else{
--- webapp/js/services/equipo-service.js
+++ webapp/js/services/equipo-service.js
@@ -282,25 +282,21 @@ var self ={
                        return d.promise;	 
 	
 				},
-				insertarPila : function(datos){
+				insertarSprint : function(datos){
 					var d = $q.defer();
 					console.log(datos);
                     $http({
                       method: 'POST',
 					  	url: 'http://192.168.1.2/Web/jscrum/api/public/sprint/',
-                        // url: 'http://localhost/gitgrad/APIPOLLO/public/observation/read/',
                         data:{
 								_codigo:datos.codigo,
-								_fecha_entrega:datos.historia,
-								_id_proyecto:datos.importancia
+								_fecha_entrega:datos.fechaEntrega,
+								_id_proyecto:datos.idProyecto
 						}
                     	})
                         .then(function successCallback(response) {
-                                // ok
-                                // self.cargado		= true;
-    							// self.cargando		= false;
+
 								self.response 	= response.data;
-								// console.log("Service"+response);
 								return d.resolve()	
                             }, function errorCallback(response) {
 								
