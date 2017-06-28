bb78b96b5cba0c8e06d271bc9f0b55168cc6e1c8
Pollo.loco <erwinXZ@hotmail.com>
2017-06-28 16:12:37 -0400

    Listo

--- jscrum (4).sql
+++ jscrum (4).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 28-06-2017 a las 06:49:25
+-- Tiempo de generación: 28-06-2017 a las 22:07:18
 -- Versión del servidor: 10.1.13-MariaDB
 -- Versión de PHP: 7.0.8
 
@@ -250,7 +250,7 @@ END$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `listarEquiposMi` (IN `_id_miembro` INT)  BEGIN
 	    IF ( SELECT EXISTS (SELECT * FROM equipo_miembro WHERE id_miembro = _id_miembro))THEN
-			SELECT * FROM equipo e INNER JOIN equipo_miembro em ON e.id=em.id_equipo WHERE em.id_miembro = _id_miembro;
+			SELECT  e.id, e.nombre, e.jornada, em.id AS id_equipo, em.cargo FROM equipo e INNER JOIN equipo_miembro em ON e.id=em.id_equipo WHERE em.id_miembro = _id_miembro;
    		ELSE
 			SELECT 0 as respuesta;
 		END IF; 
@@ -269,9 +269,15 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `listarEquiposProyectos` (IN `_id_ma
 END$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `listarEsfuerzo` (IN `_id_sprint` INT)  BEGIN
-	IF EXISTS(SELECT EXISTS (SELECT * FROM tarea WHERE id_sprint=_id_sprint))THEN
-		SELECT table1.id_tarea, group_concat(table1.cantidad separator ',') cantidades FROM (SELECT e.id, e.id_tarea, e.cantidad, e.fecha_dia FROM tarea t INNER JOIN esfuerzo e ON t.id=e.id_tarea WHERE t.id_sprint=_id_sprint
-        ORDER BY e.cantidad) table1 GROUP BY table1.id_tarea;
+DECLARE _id_tarea INT;
+	IF (SELECT EXISTS (SELECT * FROM tarea WHERE id_sprint=_id_sprint))THEN
+		IF (SELECT EXISTS (SELECT * FROM tarea t INNER JOIN esfuerzo e ON t.id=e.id_tarea WHERE id_sprint=_id_sprint))THEN
+			SELECT  table1.id, table1.indice, table1.codigo, table1.descripcion, table1.tipo, table1.estado, table1.total_horas, table1.id_miembro,  table1.id, table1.cantidad, table1.fecha_dia, table1.id_tarea, group_concat(table1.cantidad separator ',') cantidades 
+			FROM (SELECT t.id, t.indice, t.codigo, t.descripcion, t.tipo, t.estado, t.total_horas, t.id_miembro, e.id_tarea, e.cantidad, e.fecha_dia FROM tarea t INNER JOIN esfuerzo e ON t.id=e.id_tarea WHERE t.id_sprint=_id_sprint
+			ORDER BY e.cantidad) table1 GROUP BY table1.id_tarea;
+		ELSE
+			SELECT * FROM tarea WHERE id_sprint=_id_sprint;
+        END IF;
 	ELSE
 		SELECT 0 AS respuesta;
     END IF;
@@ -296,8 +302,8 @@ DECLARE _id_equipo INT;
 END$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `listarMiembrosEquipo` (IN `_id_equipo` INT)  BEGIN
-	IF ( SELECT EXISTS (SELECT * FROM equipo WHERE id = _id_equipo))THEN
-        SELECT m.id, u.nombre, u.apellidos, e.cargo FROM equipo_miembro e INNER JOIN miembro m ON e.id_miembro=m.id INNER JOIN usuario u ON m.id_usuario=u.id WHERE e.id_equipo=_id_equipo;
+	IF ( SELECT EXISTS (SELECT * FROM equipo_miembro WHERE id_equipo = _id_equipo))THEN
+        SELECT m.id, u.nombre, u.apellidos, e.cargo, m.destrezas FROM equipo_miembro e INNER JOIN miembro m ON e.id_miembro=m.id INNER JOIN usuario u ON m.id_usuario=u.id WHERE e.id_equipo=_id_equipo;
 	ELSE
 		SELECT 0 as respuesta;
 	END IF; 
@@ -459,6 +465,16 @@ CREATE TABLE `equipo_miembro` (
   `id_miembro` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
+--
+-- Volcado de datos para la tabla `equipo_miembro`
+--
+
+INSERT INTO `equipo_miembro` (`id`, `cargo`, `id_equipo`, `id_miembro`) VALUES
+(18, 'Responsable de diseño', 13, 15),
+(22, 'Sin Cargo', 12, 15),
+(25, 'Developer', 12, 18),
+(26, 'Responsable de base de datos', 12, 19);
+
 -- --------------------------------------------------------
 
 --
@@ -471,6 +487,16 @@ CREATE TABLE `esfuerzo` (
   `fecha_dia` date NOT NULL,
   `id_tarea` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `esfuerzo`
+--
+
+INSERT INTO `esfuerzo` (`id`, `cantidad`, `fecha_dia`, `id_tarea`) VALUES
+(47, 2, '2017-06-28', 12),
+(50, 1, '0000-00-00', 12),
+(51, 2, '0000-00-00', 13),
+(52, 2, '0000-00-00', 13);
 
 -- --------------------------------------------------------
 
@@ -489,9 +515,9 @@ CREATE TABLE `manager` (
 --
 
 INSERT INTO `manager` (`id`, `experiencia`, `id_usuario`) VALUES
-(10, '5', 58),
 (11, '2', 61),
-(12, '3', 57);
+(12, '3', 57),
+(13, '4', 63);
 
 -- --------------------------------------------------------
 
@@ -501,7 +527,7 @@ INSERT INTO `manager` (`id`, `experiencia`, `id_usuario`) VALUES
 
 CREATE TABLE `miembro` (
   `id` int(11) NOT NULL,
-  `destrezas` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
+  `destrezas` varchar(100) COLLATE utf8_spanish2_ci DEFAULT NULL,
   `estado` varchar(15) COLLATE utf8_spanish2_ci NOT NULL DEFAULT 'A',
   `id_usuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
@@ -511,7 +537,12 @@ CREATE TABLE `miembro` (
 --
 
 INSERT INTO `miembro` (`id`, `destrezas`, `estado`, `id_usuario`) VALUES
-(15, 'Front-end, Interfaces con AngularJS', 'A', 62);
+(15, 'Front-end, Interfaces con AngularJS', 'A', 62),
+(16, 'Diseñadora de base de datos no relacionales, Backend', 'A', 60),
+(17, 'Analista de Sistemas', 'A', 59),
+(18, 'Testing', 'A', 58),
+(19, 'Especialista en Diseño de interfaces responsivas', 'A', 65),
+(20, 'Desarrollo de aplicaciones Web y android', 'A', 64);
 
 -- --------------------------------------------------------
 
@@ -539,7 +570,12 @@ INSERT INTO `pila` (`id`, `codigo`, `historia`, `importancia`, `estimado_horas`,
 (20, 'B004', 'Tanto docente como alumno deberán poder registrarse en el sistema para poder acceder', '2', 4, 25),
 (21, 'B005', 'El docente podrá modificar o eliminar observaciones del documento', '4', 16, 25),
 (22, 'B006', 'Realizar reportes de todas las observaciones realizadas en el documento', '4', 16, 25),
-(23, 'B007', 'Traspaso de documento entre docentes', '3', 10, 25);
+(23, 'B007', 'Traspaso de documento entre docentes', '3', 10, 25),
+(24, 'H100', 'El sistema deberá tener una interfaz donde se insertarán todos los productos de la farmacia', '5', 48, 26),
+(25, 'H200', 'El sistema deberá tener una funcionalidad de búsqueda de artículos', '4', 24, 26),
+(26, 'H250', 'El sistema deberá registrar a los administradores del sistema y los usuarios', '2', 16, 26),
+(27, 'H150', 'El sistema deberá registrar las compras realizadas', '5', 36, 26),
+(28, 'H255', 'El sistema deberá registrar reportes diarios, semanales y mensuales de las ventas realizadas', '3', 36, 26);
 
 -- --------------------------------------------------------
 
@@ -564,7 +600,8 @@ CREATE TABLE `proyecto` (
 --
 
 INSERT INTO `proyecto` (`id`, `codigo`, `nombre`, `descripcion`, `objetivo`, `fecha`, `estado`, `id_manager`, `id_equipo`) VALUES
-(25, 'P001', 'Gitgrad', 'Aplicación de escritorio para versionar documentos digitales', 'Desarrollar una aplicación que permita crear versiones de un documento word para una revisión contínua', '2017-06-28 04:08:07', 'En proceso', 11, 12);
+(25, 'P001', 'Gitgrad', 'Aplicación de escritorio para versionar documentos digitales', 'Desarrollar una aplicación que permita crear versiones de un documento word para una revisión contínua', '2017-06-28 04:08:07', 'En proceso', 11, 12),
+(26, 'P250', 'FarmaHost', 'Aplicación Web para la gestión de una farmacia', 'Gestionar de manera eficiente las actividades realizadas y los artículos existentes en una farmacia', '2017-06-28 05:20:44', 'En proceso', 11, 12);
 
 -- --------------------------------------------------------
 
@@ -589,7 +626,12 @@ CREATE TABLE `proyecto_sprint` (
 
 INSERT INTO `proyecto_sprint` (`id`, `indice`, `codigo`, `historia`, `importancia`, `estimado_horas`, `id_sprint`, `id_proyecto`) VALUES
 (44, '1.1', 'S004', 'El docente deberá registrar observaciones embebidas al documento Word del estudiante', '5', 16, 12, 25),
-(45, '1.2', 'S005', 'El docente deberá registrar observaciones y guardarlas en la base de datos', '5', 8, 12, 25);
+(45, '1.2', 'S005', 'El docente deberá registrar observaciones y guardarlas en la base de datos', '5', 8, 12, 25),
+(46, '3', 'C400', 'El alumno deberá recuperar las observaciones en su documento Word para poder revisarlo', '4', 8, 13, 25),
+(47, '4', 'C401', 'El docente deberá modificar o eliminar observaciones en un documento', '4', 16, 13, 25),
+(48, '5', 'D444', 'El sistema deberá realizar reportes de las observaciones realizadas en el documento de un alumno registrado', '4', 16, 14, 25),
+(49, '6', 'D440', 'Un docente podrá pasar un documento que debía revisar a otro docente para finalizar su revisión', '3', 10, 14, 25),
+(50, '7', 'D455', 'Tanto docentes como estudiantes deberán registrarse en el sistema y previamente acceder a él medinte un login', '2', 4, 14, 25);
 
 -- --------------------------------------------------------
 
@@ -628,7 +670,10 @@ CREATE TABLE `sprint` (
 --
 
 INSERT INTO `sprint` (`id`, `codigo`, `total_horas`, `fecha_inicio`, `fecha_entrega`, `total_tareas`, `total_horas_persona`, `estado`, `id_proyecto`) VALUES
-(12, 'SP20', 24, '2017-06-28', '2017-06-28', 0, 0, 'En proceso', 25);
+(12, 'SP20', 24, '2017-06-21', '2017-06-28', 1, 8, 'En proceso', 25),
+(13, 'SP25', 24, '2017-06-29', '2017-07-09', 1, 12, 'En proceso', 25),
+(14, 'SP30', 30, '2017-07-10', '2017-07-16', 0, 0, 'En proceso', 25),
+(15, 'S35', 0, '2017-06-28', '2017-06-30', 0, 0, 'En proceso', 25);
 
 -- --------------------------------------------------------
 
@@ -648,6 +693,15 @@ CREATE TABLE `tarea` (
   `id_miembro` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
+--
+-- Volcado de datos para la tabla `tarea`
+--
+
+INSERT INTO `tarea` (`id`, `indice`, `codigo`, `descripcion`, `tipo`, `estado`, `total_horas`, `id_sprint`, `id_miembro`) VALUES
+(12, '1.1', 'T400', 'desarrollo de funcionalidad para el registro de observaciones en un documento Word', 'Principal', 'En proceso', 8, 12, 15),
+(13, '1.2', 'F002', 'Desarrollo de la funcionalidad', 'Principal', 'En proceso', 8, 12, 15),
+(15, '3', 'S001', 'Visualizacion de datos', 'Principal', 'En proceso', 12, 13, 18);
+
 -- --------------------------------------------------------
 
 --
@@ -663,7 +717,7 @@ CREATE TABLE `usuario` (
   `password` varchar(35) COLLATE utf8_spanish2_ci NOT NULL,
   `profesion` varchar(40) COLLATE utf8_spanish2_ci NOT NULL,
   `rol` varchar(45) COLLATE utf8_spanish2_ci NOT NULL DEFAULT 'Usuario',
-  `foto` varchar(400) COLLATE utf8_spanish2_ci NOT NULL DEFAULT '/images/user.svg'
+  `foto` varchar(400) COLLATE utf8_spanish2_ci NOT NULL DEFAULT 'user.svg'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
@@ -671,13 +725,16 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `nombre`, `apellidos`, `email`, `login`, `password`, `profesion`, `rol`, `foto`) VALUES
-(55, 'Erwin', 'Méndez', 'erwinXYZ1@gmail.com', 'admin', '1234', 'Ingeniero de Sistemas', 'Administrador', '/images/user.svg'),
-(57, 'Amancaya Noelia', 'Iriarte Negrón', 'kaya@gmail.com', 'Amancaya', '1234', 'Ingeniera de Sistemas', 'Manager', '/images/user.svg'),
-(58, 'Fabian', 'Janko Escalante', 'fabian@gmail.com', 'fabian', '1234', 'Ingeniero de Sistemas', 'Usuario', '/images/user.svg'),
-(59, 'Yanin', 'Oquendo Tejerina', 'yanin@gmail.com', 'yanin', '1234', 'Administrador de Empresas', 'Usuario', '/images/user.svg'),
-(60, 'Jacqueline', 'Méndez Mejía', 'jacqueline@gmail.com', 'jacqueline', '1234', 'Ingeniera de Sistemas', 'Usuario', '/images/user.svg'),
-(61, 'Gary David', 'Guzman', 'gary@gmail.com', 'gary', '1234', 'Ingeniero de Sistemas', 'Manager', '/images/user.svg'),
-(62, 'Analy', 'Polares Daza', 'analy@gmail.com', 'analy', '1234', 'Ingeniera de Sistemas', 'Usuario', '/images/user.svg');
+(55, 'Erwin', 'Méndez', 'erwinXYZ1@gmail.com', 'admin', '1234', 'Ingeniero de Sistemas', 'Administrador', 'user.svg'),
+(57, 'Amancaya Noelia', 'Iriarte Negrón', 'kaya@gmail.com', 'Amancaya', '1234', 'Ingeniera de Sistemas', 'Manager', 'user.svg'),
+(58, 'Fabian', 'Janko Escalante', 'fabian@gmail.com', 'fabian', '1234', 'Ingeniero de Sistemas', 'Usuario', 'user.svg'),
+(59, 'Yanin', 'Oquendo Tejerina', 'yanin@gmail.com', 'yanin', '1234', 'Administrador de Empresas', 'Usuario', 'user.svg'),
+(60, 'Jacqueline', 'Méndez Mejía', 'jacqueline@gmail.com', 'jacqueline', '1234', 'Ingeniera de Sistemas', 'Usuario', 'user.svg'),
+(61, 'Gary David', 'Guzman', 'gary@gmail.com', 'gary', '1234', 'Ingeniero de Sistemas', 'Manager', 'user.svg'),
+(62, 'Analy', 'Polares Daza', 'analy@gmail.com', 'analy', '1234', 'Ingeniera de Sistemas', 'Usuario', 'user.svg'),
+(63, 'Benjamin', 'Castillo Egues', 'benjamin@gmail.com', 'benjamin', '1234', 'Ingeniero de Sistemas', 'Manager', 'user.svg'),
+(64, 'Juan Pablo', 'Siles Gutierrez', 'juanpa@gmail.com', 'juan', '1234567', 'Ingeniero de Sistemas', 'Usuario', 'user.svg'),
+(65, 'Israel', 'Gonzales', 'israel@gmail.com', 'israel', '1234567', 'Ingeniero de Sistemas', 'Usuario', 'user.svg');
 
 --
 -- Índices para tablas volcadas
@@ -775,42 +832,42 @@ ALTER TABLE `dias_inhabiles`
 -- AUTO_INCREMENT de la tabla `equipo`
 --
 ALTER TABLE `equipo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 --
 -- AUTO_INCREMENT de la tabla `equipo_miembro`
 --
 ALTER TABLE `equipo_miembro`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 --
 -- AUTO_INCREMENT de la tabla `esfuerzo`
 --
 ALTER TABLE `esfuerzo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 --
 -- AUTO_INCREMENT de la tabla `manager`
 --
 ALTER TABLE `manager`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT de la tabla `miembro`
 --
 ALTER TABLE `miembro`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 --
 -- AUTO_INCREMENT de la tabla `pila`
 --
 ALTER TABLE `pila`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 --
 -- AUTO_INCREMENT de la tabla `proyecto`
 --
 ALTER TABLE `proyecto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 --
 -- AUTO_INCREMENT de la tabla `proyecto_sprint`
 --
 ALTER TABLE `proyecto_sprint`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 --
 -- AUTO_INCREMENT de la tabla `seguimiento`
 --
@@ -820,17 +877,17 @@ ALTER TABLE `seguimiento`
 -- AUTO_INCREMENT de la tabla `sprint`
 --
 ALTER TABLE `sprint`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT de la tabla `tarea`
 --
 ALTER TABLE `tarea`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
